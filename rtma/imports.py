edc206df68befbe01eff27bf66002b3e68607e30
John Hartquist <john@hartquist.com>
2020-06-18 17:00:53 -0700

    saving

--- rtma/imports.py
+++ rtma/imports.py
@@ -5,6 +5,6 @@ import pandas as pd
 from scipy.io import wavfile
 from scipy.signal import get_window, check_COLA
 from IPython.display import Audio, display
-from typing import Optional
+from typing import Optional, List
 
 np.set_printoptions(precision=4)
